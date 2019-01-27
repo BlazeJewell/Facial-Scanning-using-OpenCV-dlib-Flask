87591e554a45e3230c9ac0a5a87ba0ebe58f761e
Jake <33731110+BlazeJewell@users.noreply.github.com>
2019-01-27 09:44:03 -0500

    Cleaned up the code

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-#
-# Project: Video Streaming with Flask
-# Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
-# Date: 2014/12/21
-# Website: http://www.chioka.in/
-# Description:
-# Modified to support streaming out with webcams, and not just raw JPEGs.
-# Most of the code credits to Miguel Grinberg, except that I made a small tweak. Thanks!
-# Credits: http://blog.miguelgrinberg.com/post/video-streaming-with-flask
-#
-# Usage:
 from flask import Flask, render_template, Response
 from camera import VideoCamera
 import cv2
@@ -18,7 +6,6 @@ from imutils import face_utils
 import dlib
 
 app = Flask(__name__)
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 @app.route('/')
 def index():
@@ -38,7 +25,7 @@ def gen(camera):
         frame = camera.get_frame()
        
         yield (b'--frame\r\n'
-               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
